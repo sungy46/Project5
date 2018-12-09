a61483d7499dcf57fb7ad792e2f380ea464344ac
sungy46 <43043818+sungy46@users.noreply.github.com>
2018-12-09 15:04:52 -0500

    Add files via upload

--- Package.java
+++ Package.java
@@ -12,7 +12,7 @@ public class Package {
     private double price;
     private ShippingAddress destination;
 
-    public Package () { //default constructor
+    public Package() { //default constructor
         this.id = "";
         this.product = "";
         this.weight = 0;
@@ -20,7 +20,7 @@ public class Package {
         this.destination = new ShippingAddress();
     }
 
-    public Package (String id, String product, double weight, double price, ShippingAddress destination) {
+    public Package(String id, String product, double weight, double price, ShippingAddress destination) {
         this.id = id; //id number of product
         this.product = product; //name of product
         this.weight = weight; //weight of package
@@ -35,79 +35,80 @@ public class Package {
     public String getID() {
         return this.id;
     }
-    
+
     /**
      * @return Name of product in package
      */
     public String getProduct() {
-    	return this.product;
+        return this.product;
     }
 
     /**
      * @param product the product name to set
      */
     public void setProduct(String product) {
-    	this.product = product;
+        this.product = product;
     }
-    
+
     /**
      * @return price of product in package
      */
     public double getPrice() {
-    	return this.price;
+        return this.price;
     }
-    
+
     /**
      * @param price the price to set
      */
     public void setPrice(double price) {
-    	this.price = price;
+        this.price = price;
     }
-    
+
     /**
      * @return Package weight
      */
     public double getWeight() {
-    	return this.weight;
+        return this.weight;
     }
 
     /**
      * @param weight the weight to set
      */
     public void setWeight(double weight) {
-    	this.weight = weight;
+        this.weight = weight;
     }
-    
+
     /**
      * @return The shipping address of package
      */
     public ShippingAddress getDestination() {
-    	return this.destination;
+        return this.destination;
     }
-    
+
     /**
      * @param destination the shipping address to set
      */
-    public void setDestination (ShippingAddress destination) {
+    public void setDestination(ShippingAddress destination) {
         this.destination = destination;
     }
-    
+
     /**
      * @return The package's shipping label.
      */
 
     public String shippingLabel() {
-    	String dash = "====================";
-    	NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
-    	String price = nf.format(this.price);
-    	String weight = String.format("%.2f", this.weight);
+        String dash = "====================";
+        NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+        String price = nf.format(this.price);
+        String weight = String.format("%.2f", this.weight);
+        String longDash = "==============================";
 
-    	String label = (dash + "\n" + "TO: " + destination.getName() + "\n" +
+        String label = (dash + "\n" + "TO: " + destination.getName() + "\n" +
                 destination.getAddress() + "\n" +
                 destination.getCity() + ", " + destination.getState() + ", " + destination.getZipCode() + "\n" +
                 "Weight: " + "         " + weight + "\n" +
                 "Price: " + "        " + price + "\n" +
-                "Product:" + this.product + "\n" + dash);
-    	return label;
+                "Product: " + this.product + "\n" + dash + "\n" + longDash + "\n");
+        return label;
     }
 }
